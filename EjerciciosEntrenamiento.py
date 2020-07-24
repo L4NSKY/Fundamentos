8ed692b0931d1a1d445f50c546043a96b6689451
Lorenzo-Jacome <lorenzo.jacome@hotmail.com>
2020-07-23 21:28:30 -0500

    Ejercico mym

--- EjerciciosEntrenamiento.py
+++ EjerciciosEntrenamiento.py
@@ -36,6 +36,55 @@ def con_N_letras(lista, n):
             counter+=1
     return counter
 
+#Def reto 3
+def empiezan_Con(listaStrings, letra):
+    contadorLetra = 0
+
+    for i in range(len(listaStrings)):
+        word = listaStrings[i]
+        if word[0] == letra:
+            contadorLetra += 1
+    return contadorLetra
+
+#Def reto 4
+def duplica(listaInts):
+    dobles = []
+    for i in listaInts:
+        dobles.append(i*2)
+    return dobles
+
+#Def reto 5
+def locura(palabra):
+    newPalabra = ""
+    for i in palabra:
+        if i == "e":
+            newPalabra += '3'
+        elif i == 'o':
+            newPalabra += 'h'
+        elif i == "a":
+            newPalabra += '4'
+        else:
+            newPalabra += i
+    return newPalabra
+
+#Def reto 6
+def vocales_Mayusculas(aCambiar):
+    newWord = ""
+    for i in aCambiar:
+        if i == 'a':
+            newWord += 'A'
+        elif i == 'e':
+            newWord += 'E'
+        elif i == 'i':
+            newWord += 'I'
+        elif i == 'o':
+            newWord += 'O'
+        elif i == 'u':
+            newWord += 'U'
+        else:
+            newWord += i
+    return newWord
+
 while opcion != 11:
     print("\nMENU FUNCIONES\nIngrese el numero de la opcione que desea: ")
     print("1.Elementos en una lista mayores a N\n2.N letras o mas en un string\n3.\n4.\n5.\n11.Salir")
@@ -56,5 +105,26 @@ while opcion != 11:
         print("En la lista dada hay", cuantas, "palabras que tienen", num, "o mas letras")
 
     elif opcion == 3:
+        listaPalabras = []
+        cantidadPalabras = int(input("Dime cuantas palabras quieres ingresar: "))
+        letraContar = input("Dame la letra que quieres contar al inicio de las palabras: ")
 
+        for i in range(cantidadPalabras):
+            listaPalabras.append(input("Dame la palabra: "))
+        aparece = empiezan_Con(listaPalabras, letraContar)
+        print("La cantidad de palabras que empiezan con ", letraContar, " son: ", aparece)
 
+    elif opcion == 4:
+        listaInts = []
+        cantidadInts = int(input("¿Cuántos enteros vas a ingresar?"))
+        for i in range(cantidadInts):
+            listaInts.append(int(input("Dame un entero: ")))
+        duplicados = duplica(listaInts)
+        print(duplicados)
+    elif opcion == 5:
+        palabra = input("Dame una palabra: ")
+        newPalabra = locura(palabra)
+        print(newPalabra)
+    elif opcion == 6:
+        word = input("Dame la palabra que quieres hacer sus vocales mayusculas")
+        print(vocales_Mayusculas(word))
